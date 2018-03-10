5ebe88cb303cf22a42dcfdb094836e78139750de
Daniel Ehrhardt <daniel.ehrhardt@bussmann-it.de>
2018-03-10 13:27:06 +0100

    Updated to 0.0.3

--- dist/types/components/gradient-button/gradient-button.d.ts
+++ dist/types/components/gradient-button/gradient-button.d.ts
@@ -1,24 +1,4 @@
 import '../../stencil.core';
-import '../../stencil.core';
-import '../../stencil.core';
-import '../../stencil.core';
-import '../../stencil.core';
-import '../../stencil.core';
-import '../../stencil.core';
-import '../../stencil.core';
-import '../../stencil.core';
-import '../../stencil.core';
-import '../../stencil.core';
-import '../../stencil.core';
-import '../../stencil.core';
-import '../../stencil.core';
-import '../../stencil.core';
-import '../../stencil.core';
-import '../../stencil.core';
-import '../../stencil.core';
-import '../../stencil.core';
-import '../../stencil.core';
-import '../../stencil.core';
 export interface IColor {
     start: string;
     end: string;
